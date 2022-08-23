3755e45cf45e9b8281888a2850d1a4ad66fe6009
aguiluxo91 <apuertadelaguila@gmail.com>
2022-08-24 00:34:02 +0200

    fix: text color fixed

--- components/FuelStations.js
+++ components/FuelStations.js
@@ -49,26 +49,25 @@ export default function FuelStations() {
         <div>
             {fuelStations.map((fuelStation, i) => (
                 <div key={i} className="card m-3">
-                    <div className="card-header">
+                    <div className="card-header text-dark">
                         {fuelStation.Rótulo}
                     </div>
                     <div className="card-body">
                         <blockquote className="blockquote mb-0">
-                            <p className='fw'>{capitalizeFirstLetter(fuelStation.Dirección.toLowerCase())}</p>
+                            <p className='text-dark'>{capitalizeFirstLetter(fuelStation.Dirección.toLowerCase())}</p>
                             <p className='text-muted fs-6'>{fuelStation.Horario}</p>
                             {fuelStation["Precio Gasoleo A"] &&
-                                <p className='fw-light fs-6'>Precio Gasoleo A <span className='fw-semibold'>{fuelStation["Precio Gasoleo A"]} €</span></p>
+                                <p className='fw-light fs-6 text-dark'>Precio Gasoleo A <span className='fw-semibold'>{fuelStation["Precio Gasoleo A"]} €</span></p>
                             }
 
                             {fuelStation["Precio Gasolina 95 E5"] &&
-                                <p className='fw-light fs-6'>Precio Gasolina 95 <span className='fw-semibold'>{fuelStation["Precio Gasolina 95 E5"]} €</span></p>
+                                <p className='fw-light fs-6 text-dark'>Precio Gasolina 95 <span className='fw-semibold'>{fuelStation["Precio Gasolina 95 E5"]} €</span></p>
                             }
                             <footer className="blockquote-footer mt-3">{fuelStation.Municipio}, {fuelStation.Provincia}</footer>
                         </blockquote>
                     </div>
                 </div>
             ))}
-            {console.log(fuelStations)}
         </div>
     )
 }
