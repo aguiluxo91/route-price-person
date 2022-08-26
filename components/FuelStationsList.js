b7d2cc7367a4a937b86fb719a8e3e079f721b057
aguiluxo91 <apuertadelaguila@gmail.com>
2022-08-27 01:06:10 +0200

    fix: 1 form for each search

--- components/FuelStationsList.js
+++ components/FuelStationsList.js
@@ -1,12 +1,37 @@
 import { useEffect, useState } from 'react';
 import FuelStation from './FuelStation';
 
+const validations = {
+    cp: (value) => {
+        let message
+        if (value < 5) {
+            message = 'A Postal Code has 5 digits'
+        }
+        return message
+    },
+    city: (value) => {
+        let message
+        if (value <= 3) {
+            message = 'City must have more than 3 letters'
+        }
+        return message
+    }
+}
+
 export default function FuelStationsList() {
     const [state, setState] = useState({
         fuelStations: [],
         loading: false,
-        filter: '',
-        filteredFuelStations: []
+        filter: {
+            cp: "",
+            city: ""
+        },
+        filteredFuelStations: [],
+        errors: {
+            cp: validations.cp(),
+            city: validations.city(),
+        },
+        noResults: false
     })
 
 
@@ -23,14 +48,14 @@ export default function FuelStationsList() {
             const fuelStations = await fetch("https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/")
                 .then(fuelStations => fuelStations.json())
             if (!isUnmounted) {
-                setState({
+                setState(state => ({
+                    ...state,
                     fuelStations: fuelStations.ListaEESSPrecio,
                     loading: false
-                })
+                }))
             }
         }
         let isUnmounted = false;
-
 
         fetchFuelStations();
 
@@ -41,55 +66,114 @@ export default function FuelStationsList() {
     }, []);
 
     const handleFilterChange = (event) => {
-        const { value } = event.target
+        const { value, name } = event.target
         setState(state => ({
             ...state,
-            filter: value
+            filter: {
+                ...state.filter,
+                [name]: value
+            },
+            errors: {
+                ...state.errors,
+                [name]: validations[name] && validations[name](value)
+            }
         }))
     }
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        const { fuelStations } = state
-        const filteredFuelStations = fuelStations.filter(fuelStation => fuelStation["C.P."] === filter)
-        filteredFuelStations.sort((a, b) => Number(a["Precio Gasoleo A"].replace(/,/g, ".")) - Number(b["Precio Gasoleo A"].replace(/,/g, ".")))
-        setState(state => ({
-            ...state,
-            filteredFuelStations
-        }))
-
+        const { fuelStations, filter } = state
+        let filteredFuelStations
+        if (filter.cp.length === 5 && !filter.city) {
+            const filterByCP = fuelStations.filter(fuelStation => fuelStation["C.P."] === filter.cp)
+            filteredFuelStations = filterByCP.sort((a, b) => Number(a["Precio Gasoleo A"].replace(/,/g, ".")) - Number(b["Precio Gasoleo A"].replace(/,/g, ".")))
+        }
+        if (filter.city.length > 3 && !filter.cp) {
+            const filterByCity = fuelStations.filter(fuelStation => fuelStation.Municipio.includes(filter.city))
+            filteredFuelStations = filterByCity.sort((a, b) => Number(a["Precio Gasoleo A"].replace(/,/g, ".")) - Number(b["Precio Gasoleo A"].replace(/,/g, ".")))
+        }
+        if (filteredFuelStations.length > 1) {
+            setState(state => ({
+                ...state,
+                errors: {},
+                noResults: false,
+                filteredFuelStations
+            }))
+        } else {
+            setState(state => ({
+                ...state,
+                noResults: true
+            }))
+        }
+        
     }
 
-    const isValid = () => {
+
+    const isValid = (error) => {
         const { filter } = state
-        if ((filter && filter.length !== 5) || !filter ) return true
+        if (error === 'cp') {
+            if (filter.cp.length === 5) {
+                return true
+            }
+        }
+        if (error === 'city') {
+            if (filter.city.length > 3) {
+                return true
+            }
+        }
+        
     }
 
 
-    const { filteredFuelStations, filter, loading } = state;
+    const { filteredFuelStations, filter, loading, errors, noResults } = state;
 
     return (
         <>
             {loading && <div className="ping"></div>}
             {!loading &&
-                <form onSubmit={handleSubmit} className="mx-auto my-3 col-6 col-md-2 d-flex flex-column">
-                    <div className='input-group mb-3'>
-                        <span htmlFor="CP" className='input-group-text'><svg height="21" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"><circle cx="8.5" cy="8.5" r="5"/><path d="m17.571 17.5-5.571-5.5"/></g></svg></span>
-                        <input
-                        type="number"
-                        className='form-control'
-                        placeholder='Postal Code'
-                        onChange={handleFilterChange}
-                        value={filter}
-                        id='CP'
-                        name='CP'
-                    />
-                    </div>
-                    
-                    <button className='btn btn-primary mx-auto' disabled={isValid()}>Search Fuel Stations</button>
-                </form>
+                <>
+                    <form onSubmit={handleSubmit} className="mx-auto my-3 col-6 col-md-4 rounded">
+                        <div className='input-group mb-3'>
+                            <span htmlFor="cp" className='input-group-text'><svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" transform="translate(3 4)"><path d="m2.5.5h10c1.1045695 0 2 .8954305 2 2v8c0 1.1045695-.8954305 2-2 2h-10c-1.1045695 0-2-.8954305-2-2v-8c0-1.1045695.8954305-2 2-2z"/><path d="m10.5 2.5h1c.5522847 0 1 .44771525 1 1v1c0 .55228475-.4477153 1-1 1h-1c-.55228475 0-1-.44771525-1-1v-1c0-.55228475.44771525-1 1-1z"/><path d="m2.5 7.5h5"/><path d="m2.5 9.5h5"/></g></svg></span>
+                            <input
+                                type="number"
+                                className={`form-control ${errors.cp && "is-invalid"}`}
+                                placeholder='Postal Code'
+                                onChange={handleFilterChange}
+                                value={filter.cp}
+                                id='cp'
+                                name='cp'
+                            />
+                            <span>
+                                <button className='btn btn-primary' disabled={!isValid('cp')}>Search by Postal Code</button>
+                            </span>
+                            <div className="invalid-feedback">{errors.cp}</div>
+                        </div>
+                    </form>
+
+                    <form onSubmit={handleSubmit} className="mx-auto my-3 col-6 col-md-4 rounded">
+                        <div className='input-group mb-3'>
+                            <span htmlFor="city" className='input-group-text'><svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" transform="translate(4 2)"><path d="m6.5 16.5407715.6311176-.7118691c.71585099-.8191184 1.36011688-1.5983525 1.93279767-2.3377022l.4733382-.6239608c1.97516433-2.6615039 2.96274653-4.77276704 2.96274653-6.33378943 0-3.33218241-2.6862915-6.03344997-6-6.03344997s-6 2.70126756-6 6.03344997c0 1.56102239.98758218 3.67228553 2.96274653 6.33378943l.4733382.6239608c.73630387.9505925 1.5909423 1.9671163 2.56391527 3.0495713z"/><circle cx="6.5" cy="6.5" r="2.5"/></g></svg></span>
+                            <input
+                                type="text"
+                                className={`form-control ${errors.city && "is-invalid"}`}
+                                placeholder='Enter your city name...'
+                                onChange={handleFilterChange}
+                                value={filter.city}
+                                id='city'
+                                name='city'
+                            />
+                            <span>
+                                <button className='btn btn-primary mx-auto' disabled={!isValid('city')}>Search by City</button>
+                            </span>
+                            <div className="invalid-feedback">{errors.city}</div>
+                        </div>
+
+                    </form>
+                </>
             }
             {filteredFuelStations && <FuelStation filteredFuelStations={filteredFuelStations} />}
+            {noResults && <p>No Results</p>}
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Journey Calculator</title>
         <meta name="Route Price Calculator" content="Calculate a route price" />
         <link rel="icon" href="/favicon.ico" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"></link>
