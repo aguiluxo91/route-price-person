0a224be79808aaa7c5b4d601c5f3edee4862aec6
aguiluxo91 <apuertadelaguila@gmail.com>
2022-08-29 17:38:32 +0200

    feat: app icon

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <Head>
         <title>Journey Calculator</title>
         <meta name="Route Price Calculator" content="Calculate a route price" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/journey-icon.png" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"></link>
       </Head>
 
